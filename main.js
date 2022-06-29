f1ff8d85924010f1edb1079c1b4246de8a82b861
Darren Iyoyo <darreniyoyo@gmail.com>
2022-06-29 10:04:37 +0200

    uploaded all images. score, time, and level added

--- main.js
+++ main.js
@@ -1,42 +1,75 @@
-//Start game function
+//start game function
+
 const start = document.getElementById("start");
 const header = document.getElementById("header");
 const answerInput = document.getElementById("answer-input");
 const checkAnswerButton = document.getElementById("check-button");
-let LevelIndex = 0;
-let LevelsPassed = 0;
+let level = document.getElementById("level-counter").innerHTML;
+let score = document.getElementById("score-count").innerHTML;
 
-console.log(LevelIndex);
+let levelIndex = 0;
+let levelsPassed = 0;
+let livesRemaining = 3;
+let scoreIndex = 0;
+
+console.log(levelIndex);
+console.log(livesRemaining);
+
+//initialize board
 
 start.addEventListener("click", initializeBoard);
 
-// const randomCountry, currentCountryIndex
-
-//initialize Board
 function initializeBoard(){
 start.classList.add("hide");
 header.classList.remove("hide");
 answerInput.classList.remove("hide");
-changeImage()
+changeImage();
+startTimer();
 }
+
+//timer
+function startTimer (){
+const intervalId = setInterval(function () {
+  let timer = document.getElementById("countdown-timer").innerHTML;
+  if (timer > 0) {
+    return timer;
+    console.log(timer);
+  } else {
+    console.log("Game Over");
+    clearInterval(intervalId);
+  }
+  timer--;
+}, 1000);
+}
+
+//level counter
+
+
+//score
+
 
 //country selector
 
 const countryImages = [
 {
-    "imgSource": "./blank-country-maps/jpg/blank+map+of+the+Netherlands.jpg",
+    "imgSource": "./images/blank+map+of+the+Netherlands.jpeg",
     "answer": "The Netherlands"
 },
 {
-    "imgSource": "./blank-country-maps/jpg/blank+map+of+germany.jpg",
+    "imgSource": "./images/blank+map+of+germany.jpeg",
     "answer": "Germany",
 
 }
 ];
 
 function changeImage() {
- let imageShown = countryImages[LevelIndex].imgSource;
+ let imageShown = countryImages[levelIndex].imgSource;
   document.getElementById("country-map").src = imageShown;
+}
+
+//page update
+function updatePage (){
+
 }
 
 //accept input
@@ -44,26 +77,28 @@ function changeImage() {
 checkAnswerButton.addEventListener("click", checkAnswer);
 
 function checkAnswer(){
-let answer = countryImages[LevelIndex].answer;
+let answer = countryImages[levelIndex].answer;
 let input = document.getElementById("guess").value;
-if (answer === input){
-console.log(true);
-LevelIndex++;
-LevelsPassed++;
-changeImage();
-} else {console.log(false);
+if (answer === input){ //find a way to accept multiple correct answers
+    console.log(true); // return correct answer
+    levelIndex++;
+    levelsPassed++;
+    initializeBoard()
+} else {
+    console.log(false); //return incorrect message
+    livesRemaining--;
 }
 }
 
+console.log(levelsPassed);
 
-//timer
+//game over --- Create a winning / game over page
+if (levelsPassed === 21){
+    console.log("You Win"); // 
+};
 
-
-//Game over
-function gameOver() {
-    if (LevelsPassed === 21)
-    console.log(gameOver)
-}
-//score
+if (livesRemaining === 0){
+        console.log("Game Over")
+    }
 
 
